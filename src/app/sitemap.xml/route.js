149574cf84632a1b94db47e8677dc5266897a68b
lipo541 <141038060+lipo541@users.noreply.github.com>
2025-07-17 07:09:00 +0400

    sitemap update

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,10 +1,9 @@
 // This file creates a proper XML sitemap for Next.js App Router
 import { NextResponse } from 'next/server'
-import sitemapData from '../sitemap'
+import sitemapData from '../sitemapData'
 
 export async function GET() {
-  const items = sitemapData()
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${items
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${sitemapData
     .map(item => `  <url>\n    <loc>${item.url}</loc>\n    <lastmod>${new Date(item.lastModified).toISOString().split('T')[0]}</lastmod>\n    <changefreq>${item.changeFrequency}</changefreq>\n    <priority>${item.priority}</priority>\n  </url>`)
     .join('\n')}\n</urlset>`
   return new NextResponse(xml, {
